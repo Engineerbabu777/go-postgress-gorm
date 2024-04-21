ce0669992fc6ab8e06e7b7106bbeacafb2a1fa7b
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-21 19:48:47 +0500

    Add: finished

--- models/models.go
+++ models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "gorm.io/gorm"
+
 type Books struct {
 	ID        uint    `gorm:"primary key;autoIncrement" json:"id"`
 	Author    *string `json:"author"`
@@ -7,11 +9,7 @@ type Books struct {
 	Publisher *string `json:"publisher"`
 }
 
-
-
-func MigrateBooks(db *gorm.DB) error{
-
-	err := db.AutoMigrate(&Books{});
-
-	return err;
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+	return err
 }
